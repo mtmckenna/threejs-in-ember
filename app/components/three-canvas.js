b85116e694541940e5a90b1d3fc7570277dcfba5
Matt McKenna <matt@mtmckenna.com>
2016-12-30 11:19:34 -0800

    fix slider

--- app/components/three-canvas.js
+++ app/components/three-canvas.js
@@ -2,104 +2,129 @@ import Ember from 'ember';
 import THREE from 'npm:three';
 import shaders from 'threejs-in-ember/ember-stringify';
 
-// TODO: resize, texture loader, css, party mode, slider, dragging, netlify, getters
+// TODO: texture loader, party mode, dragging, netlify, getters, rename to box, ember-concurrency
 
 export default Ember.Component.extend({
-	vertexShader: shaders['vertex.glsl'],
-	fragmentShader: shaders['fragment.glsl'],
+  vertexShader: shaders['vertex.glsl'],
+  fragmentShader: shaders['fragment.glsl'],
+  classNames: ['three-canvas'],
 
 
-	didInsertElement() {
-		Ember.run.scheduleOnce('afterRender', () => {
-			this.configureScene();
-		});
-	},
+  didInsertElement() {
+    Ember.run.scheduleOnce('afterRender', () => {
+      this.configureScene();
+    });
+  },
 
-	configureScene() {
-		this.set('startTime', Date.now());
-		this.set('time', 1.0);
+  configureScene() {
+    let glRenderer = this.get('glRenderer');
+    this.get('element').appendChild(glRenderer.domElement);
 
-		let glRenderer = this.get('glRenderer');
+    let loader = new THREE.TextureLoader();
+    loader.load('ember-logo.png', (texture) => {
+      if (this.isDestroyed || this.isDestroying) { return; }
+      this.set('texture', texture);
+      let cube = this.get('cube');
+      this.get('scene').add(cube);
+      this.animate();
+    });
 
-		glRenderer.setSize(window.innerWidth, window.innerHeight);
-		this.get('element').appendChild(glRenderer.domElement);
+  },
 
-		let loader = new THREE.TextureLoader();
-		loader.load('ember-logo.png', (texture) => {
-			this.set('texture', texture);
-			let cube = this.get('cube');
-			this.get('scene').add(cube);
-			this.animate();
-		});
+  resizeCanvas() {
+    if (!this.get('canvasShouldResize')) { return; }
+    let glRenderer = this.get('glRenderer');
+    let camera = this.get('camera');
+    let element = this.get('element');
+    let width = element.clientWidth;
+    let height = element.clientHeight;
 
-	},
+    glRenderer.setSize(width, height);
+    camera.aspect	= width / height;
+    camera.updateProjectionMatrix();
+  },
 
-	animate() {
-		requestAnimationFrame(this.animate.bind(this));
-		let cube = this.get('cube');
-		cube.rotation.x += 0.005;
-		cube.rotation.y += 0.005;
-		var elapsedMilliseconds = Date.now() - this.get('startTime');
-		var elapsedSeconds = elapsedMilliseconds / 1000.;
-		this.set('time', 60. * elapsedSeconds);
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    let cube = this.get('cube');
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
 
-		this.draw();
-	},
+    this.updateScale();
+    this.draw();
+  },
 
-	draw() {
-		let glRenderer = this.get('glRenderer');
-		let scene = this.get('scene');
-		let camera = this.get('camera');
-		glRenderer.render(scene, camera);
-	},
+  updateScale() {
+    let cube = this.get('cube');
+    let scale = this.get('scale') || 1.0;
+    cube.scale.set(scale, scale, scale);
+  },
 
-	uniforms: Ember.computed('time', function() {
-		return {
-			texture: { type: "t", value: this.get('texture') },
-			time: { type: "f", value: this.get('time') },
-			lights: true
-		};
-	}),
+  draw() {
+    this.resizeCanvas();
+    let glRenderer = this.get('glRenderer');
+    let scene = this.get('scene');
+    let camera = this.get('camera');
+    glRenderer.render(scene, camera);
+  },
 
-	scene: Ember.computed(function() {
-		return new THREE.Scene();
-	}),
+  uniforms: Ember.computed(function() {
+    return {
+      texture: { type: "t", value: this.get('texture') }
+    };
+  }),
 
-	glRenderer: Ember.computed(function() {
-		let glRenderer = new THREE.WebGLRenderer({ alpha: true });
-		return glRenderer;
-	}),
+  scene: Ember.computed(function() {
+    return new THREE.Scene();
+  }),
 
-	camera: Ember.computed(function() {
-		let camera = new THREE.PerspectiveCamera(75,
-																						 window.innerWidth / window.innerHeight,
-																						 0.1,
-																						 1000.0);
-		camera.position.z = 5;
-		return camera;
-	}),
+  glRenderer: Ember.computed(function() {
+    let glRenderer = new THREE.WebGLRenderer({ alpha: true });
+    return glRenderer;
+  }),
 
-	geometry: Ember.computed(function() {
-		var geometry = new THREE.BoxGeometry(1, 1, 1);
-		return geometry;
-	}),
+  camera: Ember.computed(function() {
+    let element = this.get('element');
+    let displayWidth  = element.clientWidth;
+    let displayHeight = element.clientHeight;
 
-	material: Ember.computed(function() {
-		let vertexShader = this.get('vertexShader');
-		let fragmentShader = this.get('fragmentShader');
-		let uniforms = this.get('uniforms');
+    let camera = new THREE.PerspectiveCamera(75,
+                                             displayWidth / displayHeight,
+                                             0.1,
+                                             1000.0);
+                                             camera.position.z = 5;
+                                             return camera;
+  }),
 
-		return new THREE.ShaderMaterial({
-			vertexShader: vertexShader,
-			fragmentShader: fragmentShader,
-			uniforms: uniforms
-		});
-	}),
+  geometry: Ember.computed(function() {
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    return geometry;
+  }),
 
-	cube: Ember.computed(function() {
-		let geometry = this.get('geometry');
-		let material = this.get('material');
-		let cube = new THREE.Mesh(geometry, material);
-		return cube;
-	})
+  material: Ember.computed(function() {
+    let vertexShader = this.get('vertexShader');
+    let fragmentShader = this.get('fragmentShader');
+    let uniforms = this.get('uniforms');
+
+    return new THREE.ShaderMaterial({
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      uniforms: uniforms
+    });
+  }),
+
+  cube: Ember.computed(function() {
+    let geometry = this.get('geometry');
+    let material = this.get('material');
+    let cube = new THREE.Mesh(geometry, material);
+    return cube;
+  }),
+
+  canvasShouldResize: Ember.computed(function() {
+    let canvas = this.get('glRenderer').domElement;
+    let element = this.get('element');
+    let displayWidth  = element.clientWidth;
+    let displayHeight = element.clientHeight;
+    return displayWidth !== canvas.width || displayHeight !== canvas.height;
+  }).volatile()
 });
