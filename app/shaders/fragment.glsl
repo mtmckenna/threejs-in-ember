b62e8dc6267e8cfd8d1b95868868e8dd4f2fcdfa
Matt McKenna <matt@mtmckenna.com>
2016-12-31 17:18:05 -0800

    improve lighting

--- app/shaders/fragment.glsl
+++ app/shaders/fragment.glsl
@@ -5,9 +5,9 @@ varying vec2 vUv;
 varying vec3 vNormal;
 
 void main() {
-  vec3 ambientLight = vec3(0.4, 0.4, 0.4);
+  vec3 ambientLight = vec3(0.0, 0.0, 0.0);
   vec3 directionalLightColor = vec3(0.5, 0.5, 0.5);
-  vec3 lightDirection = vec3(0.0, 1.0, 0.0);
+  vec3 lightDirection = vec3(0.0, 1.0, 1.0);
   float directionalLightMagnitude = max(dot(vNormal, lightDirection), 0.0);
   vec3 light = ambientLight + directionalLightColor * directionalLightMagnitude;
 
